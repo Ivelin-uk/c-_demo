a802c75504122b74e0da44f6943ad5c03a75dce0
Ivelin-uk <ivoagaininwow@gmail.com>
2024-09-29 09:54:31 +0300

    add

---
+++ App_07_ref/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {       
+        //Прости дипове данни 
+        //byte, ubyte, short, ushort, int, uint, long ulong, float , double , bool 
+
+        //Референтни
+        //Масиви , обектите
+
+        int[] numbers = {1,2,3,4,5,6};
+        System.Console.WriteLine(String.Join(",", numbers));
+        Update(numbers);
+        System.Console.WriteLine(String.Join(",", numbers));
+    }
+
+    public static void Update(int[] arr) 
+    {
+        arr[0] = 100; 
+    }
+}
